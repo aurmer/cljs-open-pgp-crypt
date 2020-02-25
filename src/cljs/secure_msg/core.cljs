738a0be6c4352787eac67d66bd1b8afe7dd5895e
Aubrey <aubrey.snider@gmail.com>
2020-02-25 13:40:43 -0600

    added state

--- src/cljs/secure_msg/core.cljs
+++ src/cljs/secure_msg/core.cljs
@@ -1,7 +1,11 @@
 (ns secureMsg.core
   (:require [reagent.core :as reagent]))
 
+(def app-state
+  (reagent/atom
+    {:message "Hello from App State!"}))
+
 (defn app []
-  [:h1 {:class "title"} "Hello from a Clojure data structure... not JSX... WOW"])
+  [:h1 (:message @app-state)])
 
 (reagent/render [app] (js/document.querySelector "#root"))
